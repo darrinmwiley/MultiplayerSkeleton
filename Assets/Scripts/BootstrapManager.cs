ac51b3cbc4401eaea7b05686d4492bdaba115f7e
darrinmwiley <darrinmwiley@gmail.com>
2024-06-02 00:23:52 -0700

    Initial commit with Unity .gitignore

---
+++ Assets/Scripts/BootstrapManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using FishNet.Managing;
+using Steamworks;
+
+public class BootstrapManager : MonoBehaviour
+{
+    private static BootstrapManager instance;
+    
+    private void Awake() => instance = this;
+
+    [SerializeField] private string menuName = "MainMenu";
+    [SerializeField] private NetworkManager _networkManager;
+    [SerializeField] private FishySteamworks.FishySteamworks _fishySteamworks;
+
+    protected Callback<LobbyCreated_t> LobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> JoinRequest;
+    protected Callback<LobbyEnter_t> LobbyEntered;
+
+    public static ulong CurrentLobbyID;
+
+    public static void Foo(){
+        Debug.Log("foo");
+    }
+
+    public static void CreateLobby(){
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+    }
+
+    private void Start()
+    {
+        LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        JoinRequest = Callback<GameLobbyJoinRequested_t>.Create(OnJoinRequest);
+        LobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuName, LoadSceneMode.Additive);
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        Debug.Log("starting lobby creation" + callback.m_eResult.ToString());
+        if(callback.m_eResult != EResult.k_EResultOK)
+        {
+            Debug.Log("lobby creation failed");
+            return;
+        }
+        CurrentLobbyID = callback.m_ulSteamIDLobby;
+        SteamMatchmaking.SetLobbyData(new CSteamID(CurrentLobbyID), "HostAddress", SteamUser.GetSteamID().ToString());
+        SteamMatchmaking.SetLobbyData(new CSteamID(CurrentLobbyID), "name", SteamFriends.GetPersonaName().ToString()+"'s lobby");
+        _fishySteamworks.SetClientAddress(SteamUser.GetSteamID().ToString());
+        _fishySteamworks.StartConnection(/*server = */ true);
+        Debug.Log("Lobby creation was successful");
+    }
+
+    private void OnJoinRequest(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        CurrentLobbyID = callback.m_ulSteamIDLobby;
+
+        MainMenuManager.LobbyEntered(SteamMatchmaking.GetLobbyData(new CSteamID(CurrentLobbyID), "name"), _networkManager.IsServer);
+
+        _fishySteamworks.SetClientAddress(SteamMatchmaking.GetLobbyData(new CSteamID(CurrentLobbyID), "HostAddress"));
+        //error is occurring on the line below:
+        _fishySteamworks.StartConnection(/*server = */false);
+    }
+
+    public static void JoinByID(CSteamID steamID)
+    {
+        Debug.Log("attempting to join lobby id "+steamID.m_SteamID);
+        if(SteamMatchmaking.RequestLobbyData(steamID))
+            SteamMatchmaking.JoinLobby(steamID);
+        else{
+            Debug.Log("failed to join lobby id "+steamID.m_SteamID);
+        }
+    }
+
+    public static void LeaveLobby()
+    {
+        SteamMatchmaking.LeaveLobby(new CSteamID(CurrentLobbyID));
+        CurrentLobbyID = 0;
+        instance._fishySteamworks.StopConnection(/*server =*/ false);
+        if(instance._networkManager.IsServer)
+            instance._fishySteamworks.StopConnection(/*server = */ false);
+    }
+}

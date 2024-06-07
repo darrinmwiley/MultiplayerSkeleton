c6a4a48e058d05f4d718f0afe09f4dd1495d53c5
darrinmwiley <darrinmwiley@gmail.com>
2024-06-07 11:54:04 -0700

    move Textures and Shaders into Graphics Directory

--- Assets/_LoveshackGames/Games/Codeworld/Codeworld.cs
+++ Assets/_LoveshackGames/Games/Codeworld/Codeworld.cs
@@ -2,17 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Codeworld : LSGame
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+public class Codeworld : LSGame{}
